74d6e299436f18bcaf5e9d5cad40a1587f5aa28b
Weihong Guan <weihong.guan@gmail.com>
2018-01-05 16:32:43 +0800

    basic demo

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+//% weight=70 icon="\uf1e7" color=#EC7505
+namespace sourcekit {
+
+  //% blockId=sourcekit_pick block="pick a %num"
+  export function pick(num: Number): void {
+    serial.writeNumber(num)
+    serial.writeLine("")
+  }
+}
