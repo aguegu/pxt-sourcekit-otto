53c0b496caf19cc0f5bbbbda4acabdb01dae3d22
Weihong Guan <weihong.guan@gmail.com>
2018-01-05 16:46:15 +0800

    tweak

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
-//% weight=70 icon="\uf1e7" color=#EC7505
+//% weight=70 icon="\uf1e7" color=#64b8e0
 namespace sourcekit {
 
-  //% blockId=sourcekit_pick block="pick a %num"
-  export function pick(num: Number): void {
-    serial.writeNumber(num)
-    serial.writeLine("")
-  }
+    //% blockId=sourcekit_pick block="pick a %num"
+    export function pick(num: number): void {
+        serial.writeNumber(num)
+        serial.writeLine("")
+    }
 }
